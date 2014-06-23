622b929facd333348f9090b2976f11cd984fd289
Timmy Sjöstedt <git@iostream.se>
2014-06-23 18:39:40 +0200

    Implemented Domain object.

---
+++ src/Domain.php
@@ -0,0 +1,43 @@
+<?php
+namespace brajox\Binero;
+
+class Domain extends StrictStruct
+{
+	public $DomainName;
+	public $ExpireDate;
+	public $ExpireDateShortString;
+	public $IsActive;
+
+	public function getName()
+	{
+		return $this->DomainName;
+	}
+
+	public function getExpireDate()
+	{
+		return $this->ExpireDate;
+	}
+
+	public function getExpireDateShort()
+	{
+		return $this->ExpireDateShortString;
+	}
+
+	public function isActive()
+	{
+		return $this->IsActive;
+	}
+
+	public function hasExpired($compare_time = null)
+	{
+		if (is_null($compare_time)) {
+			$compare_time = new \DateTime;
+		}
+
+		if ( ! ($compare_time instanceof \DateTime)) {
+			$compare_time = new \DateTime($compare_time);
+		}
+
+		return $compare_time >= new \DateTime($this->getExpireDate());
+	}
+}
---
+++ test/DomainTest.php
@@ -0,0 +1,47 @@
+<?php
+namespace brajox\Binero\Test;
+
+use brajox\Binero;
+
+class DomainTest extends \PHPUnit_Framework_TestCase
+{
+	private $domain_data = array(
+		'DomainName' => 'testar.se',
+		'ExpireDate' => '2015-03-22T01:00:00',
+		'ExpireDateShortString' => '2015-03-22',
+		'IsActive' => true,
+	);
+
+	public function setUp()
+	{
+		$this->Domain = new Binero\Domain($this->domain_data);
+	}
+
+	public function testDataRetainment()
+	{
+		$this->assertEquals($this->domain_data['DomainName'], $this->Domain->getName());
+		$this->assertEquals($this->domain_data['ExpireDate'], $this->Domain->getExpireDate());
+		$this->assertEquals($this->domain_data['ExpireDateShortString'], $this->Domain->getExpireDateShort());
+		$this->assertEquals($this->domain_data['IsActive'], $this->Domain->isActive());
+	}
+
+	/**
+	 * @dataProvider hasExpiredProvider
+	 */
+	public function testHasExpired($datetime, $expected_result)
+	{
+		$this->assertEquals($expected_result, $this->Domain->hasExpired($datetime));
+	}
+	public function hasExpiredProvider()
+	{
+		return array(
+			array('2014-02-02', false),
+			array('2015-03-22', false),
+			array('2015-03-22T00:59:59', false),
+			array('2015-03-22T01:00:00', true),
+			array('2015-03-23', true),
+			array(new \DateTime('2014-01-05'), false),
+			array(new \DateTime('2015-03-23'), true),
+		);
+	}
+}
