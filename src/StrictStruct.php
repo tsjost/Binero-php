42f4104d2775c5d8e5d495d3c94cdfb9a0f46969
Timmy Sjöstedt <git@iostream.se>
2014-06-23 17:45:16 +0200

    Added StrictStruct.

---
+++ src/StrictStruct.php
@@ -0,0 +1,17 @@
+<?php
+namespace brajox\Binero;
+
+abstract class StrictStruct
+{
+	public function __construct(array $defaults = array())
+	{
+		foreach ($defaults as $key => $value) {
+			$this->{$key} = $value;
+		}
+	}
+
+	public function __set($name, $value)
+	{
+		throw new \DomainException("Cannot use undeclared property '". $name ."'");
+	}
+}
---
+++ test/StrictStructTest.php
@@ -0,0 +1,61 @@
+<?php
+use brajox\Binero;
+
+class ConcreteStrictStruct extends Binero\StrictStruct
+{
+	public $declared;
+	public $foo;
+	public $bar;
+	public $baz;
+}
+
+class StrictStructTest extends PHPUnit_Framework_TestCase
+{
+	/**
+	 * @expectedException DomainException
+	 */
+	public function testUndeclared()
+	{
+		$strictStruct = new \ConcreteStrictStruct;
+
+		$strictStruct->undeclared = "property doesn't exist";
+	}
+
+	public function testDeclared()
+	{
+		$strictStruct = new \ConcreteStrictStruct;
+
+		$strictStruct->declared = "property exists";
+
+		$this->assertTrue(true);
+	}
+
+	/**
+	 * @expectedException DomainException
+	 */
+	public function testPassedUndeclared()
+	{
+		$values = array(
+			'also_undeclared' => "that doesn't exist",
+		);
+
+		new \ConcreteStrictStruct($values);
+	}
+
+	public function testPassedValues()
+	{
+		$values = array(
+			'foo' => 1337,
+			'bar' => 'FooBar',
+			'baz' => 3.14159265,
+		);
+
+		$strictStruct = new \ConcreteStrictStruct($values);
+
+		foreach ($values as $key => $value) {
+			$this->assertEquals($value, $strictStruct->{$key});
+		}
+
+		$this->assertNull($strictStruct->declared);
+	}
+}
